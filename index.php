4f643c15aba7deeeac5756ba04a40de743cd5729
PittiDavide <76527290+PittiDavide@users.noreply.github.com>
2023-03-29 23:14:45 +0200

    index e inizio prenotazioni

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>B&B Home Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="prenotazioni.php">Prenotazioni</a></li>
+				<li><a href="#">Modifica</a></li>
+				<li><a href="#">Prenota</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section class="hero">
+			<h1>Benvenuti al nostro B&B</h1>
+			<p>Un luogo accogliente dove trascorrere le vostre vacanze.</p>
+			<a href="https://youtu.be/GDOccr-qyVc">Scopri di più</a>
+		</section>
+		<section class="features">
+			<div class="feature">
+				<img src="https://www.bellavitainpuglia.it/Content/images/partner/4141/1920x0/ca3f5d8626582c6048a09181061c4614.jpg" alt="Spa">
+				<h2>Spa inclusa</h2>
+				<p>Il miglior modo per rilassarvi e godervi la vostre giornate di riposo.</p>
+			</div>
+			<div class="feature">
+				<img src="https://www.bedandbreakfastlatosca.com/images/sala-colazione-beb-la-tosca-marina-di-massa_page.jpg" alt="Colazione">
+				<h2>Colazione inclusa</h2>
+				<p>Una ricca colazione vi aspetta ogni mattina.</p>
+			</div>
+			<div class="feature">
+				<img src="https://www.bedandbreakfastsangiovanniinmarignano.com/wp-content/uploads/2020/04/il-parcheggio-del-bb-il-Casale.jpg" alt="Parcheggio">
+				<h2>Parcheggio gratuito</h2>
+				<p>Parcheggio gratuito disponibile per tutti i nostri ospiti.</p>
+			</div>
+		</section>
+		<section class="gallery">
+			<h2>La nostre camere</h2>
+			<div class="gallery-images">
+				<img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/194381287.jpg?k=68691a06101e3595754648015549fc758279d8dfd23c6ddf32e73d24d72b14c7&o=&hp=1" alt="Galleria 1">
+				<img src="https://b-galleria-frascati.hotelmix.it/data/Photos/OriginalPhoto/10009/1000963/1000963038/Galleria-Frascati-Rooms-And-Apartment-Exterior.JPEG" alt="Galleria 2">
+				<img src="https://media-cdn.tripadvisor.com/media/photo-s/09/66/d8/c4/b-b-galleria-frascati.jpg" alt="Galleria 3">
+			</div>
+		</section>
+	</main>
+	<footer>
+		<p>Tutti i diritti riservati &copy; 2023</p>
+	</footer>
+</body>
+</html>
---
+++ prenotazioni.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>B&B prenotazioni</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="prenotazioni.php">Prenotazioni</a></li>
+				<li><a href="#">Modifica</a></li>
+				<li><a href="#">Prenota</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
